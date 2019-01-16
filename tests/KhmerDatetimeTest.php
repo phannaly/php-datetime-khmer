dce2c41d378aaedeac13b319bd3becb1eca8caba
phanna <studentphanna@gmail.com>
2019-01-16 08:52:48 +0700

    Apply fixes from StyleCI (#1)

--- tests/KhmerDatetimeTest.php
+++ tests/KhmerDatetimeTest.php
@@ -5,56 +5,57 @@ use Phanna\Converter\KhmerDatetime;
 class KhmerDatetimeTest extends \PHPUnit\Framework\TestCase
 {
     public $khmer;
-    public function setUp() 
+
+    public function setUp()
     {
         $date = date('2019-01-22');
-        $this->khmer = (new KhmerDatetime)->convert($date);
+        $this->khmer = (new KhmerDatetime())->convert($date);
     }
 
-    public function test_get_full_month_in_khmer() 
+    public function test_get_full_month_in_khmer()
     {
-        $this->assertEquals("មករា",$this->khmer->getFullMonth());
+        $this->assertEquals('មករា', $this->khmer->getFullMonth());
     }
 
-    public function test_get_full_day_in_khmer() 
+    public function test_get_full_day_in_khmer()
     {
-        $this->assertEquals("២២",$this->khmer->getFullDay());
+        $this->assertEquals('២២', $this->khmer->getFullDay());
     }
 
-    public function test_get_full_year_in_khmer() 
+    public function test_get_full_year_in_khmer()
     {
-        $this->assertEquals("២០១៩",$this->khmer->getFullYear());
+        $this->assertEquals('២០១៩', $this->khmer->getFullYear());
     }
 
-    public function test_check_dash_format_date() 
+    public function test_check_dash_format_date()
     {
         $this->assertTrue($this->khmer->isDash());
     }
 
-    public function test_check_forward_slash_format() 
+    public function test_check_forward_slash_format()
     {
         $date = date('2019/01/22');
-        $khmer = (new KhmerDatetime)->convert($date);
+        $khmer = (new KhmerDatetime())->convert($date);
 
         $this->assertFalse($this->khmer->isForwardSlah());
         $this->assertTrue($khmer->isForwardSlah());
     }
 
-    public function test_get_date_in_khmer() 
+    public function test_get_date_in_khmer()
     {
-        $this->assertEquals("២០១៩-មករា-២២",$this->khmer->getDate());
+        $this->assertEquals('២០១៩-មករា-២២', $this->khmer->getDate());
     }
-    
-    public function test_get_reverse_date_in_khmer() 
-    {
-        $this->assertEquals("២២-មករា-២០១៩",$this->khmer->getDate('revers'));
-    }   
 
-    public function test_throw_exception_when_wrong_format() 
+    public function test_get_reverse_date_in_khmer()
+    {
+        $this->assertEquals('២២-មករា-២០១៩', $this->khmer->getDate('revers'));
+    }
+
+    public function test_throw_exception_when_wrong_format()
     {
         $this->expectException(\Exception::class);
 
         $date = date('2019\01\22');
-        $khmer = (new KhmerDatetime)->convert($date)->getDate();
+        $khmer = (new KhmerDatetime())->convert($date)->getDate();
     }
 }
