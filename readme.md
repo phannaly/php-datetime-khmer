07947222347ae6ffac945e632523981e2b0b6fef
phanna <studentphanna@gmail.com>
2019-01-16 08:43:47 +0700

    Initial project

---
+++ readme.md
@@ -0,0 +1,75 @@
+# The PHP helpers function extract from Laravel
+
+[![Build Status](https://travis-ci.org/phannaly/php-datetime-khmer.svg?branch=master)](https://travis-ci.org/phannaly/php-datetime-khmer)  [![StyleCI](https://github.styleci.io/repos/148669698/shield?branch=master)](https://github.styleci.io/repos/148669698)
+
+This is a small package for convert datetime to Khmer language.
+
+## Requirements
+
+* PHP 7.0 or higher
+
+## Setup
+
+You don't need to install by composer if your project doesn't have it.
+
+Just import it manually in `src` folder.
+
+But If you want to install by composer, please follow command below
+
+    composer require phannaly/php-datetime-khmer
+
+
+<a name="usage"></a>
+## Usage
+
+Whenever you want to convert any datetime into Khmer language, just wrap it inside method.
+
+Firstly, you can import or instance class
+```php
+use Phanna\Converter\KhmerDatetime;
+....
+$khmer = new Phanna\Converter\KhmerDatetime;
+```
+
+After that you can you following method below.
+
+```php
+$khmer = new KhmerDatetime;
+$date = date('Y-m-d'); // or specific date that you want
+// Example output 2019-01-22
+
+$khmer->convert($date)->getDate();
+// Output: ២០១៩-មករា-២២
+```
+### Available methods
+
+```php
+$khmer = new KhmerDatetime;
+$date = '2019-01-22';
+
+$khmer->convert($date)->getFullMonth();
+// Output: មករា
+
+$khmer->convert($date)->getFullYear();
+// Output: ២០១៩
+
+$khmer->convert($date)->getFullDay();
+// Output: ២២
+
+$date = '2019/01/22'; // forward slash format
+$khmer->convert($date)->getDate();
+// Output: ២០១៩/មករា/២២
+ 
+$date = '2019/01/22'; // forward slash format
+$khmer->convert($date)->getDate('reverse');
+// Output: ២២/មករា/២០១៩
+
+```
+
+## Contributing
+
+Feel free to contribute through PR.
+
+## License
+
+This package operates under the MIT License (MIT). See the [LICENSE](https://github.com/phannaly/php-datetime-khmer/blob/master/LICENSE.md) file for details.
---
+++ src/KhmerDatetime.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace Phanna\Converter;
+
+use DateTime;
+
+class KhmerDatetime {   
+    /**
+     * @var array
+     */
+    private $khmerNumber = [];
+    /**
+     * @var array
+     */
+    private $internationalMonth = [];
+    /**
+     * @var array
+     */
+    private $khmerMonth = [];
+    /**
+     * @var Date
+     */
+    public $date;
+
+    public function convert($date) 
+    {
+        $this->date = $date;
+        $this->khmerMonth = $this->getKhmerMonths();
+        $this->khmerNumber = $this->getKhmerNumber();
+        $this->internationalMonth = $this->getInternationalMonth();
+
+        return $this;
+    }
+
+    /**
+     * Get full month name in Khmer
+     *
+     * @return string
+     */
+    public function getFullMonth() 
+    {
+        $month = date("F",strtotime($this->date));
+
+        $monthIndex = array_search($month, $this->internationalMonth);
+
+        return $this->khmerMonth[$monthIndex];
+    }
+
+    /**
+     * Get full month name in Khmer
+     *
+     * @return string
+     */
+    public function getFullDayName() 
+    {
+        $day = date("l",strtotime($this->date));
+
+        $dayIndex = array_search($day, $this->getStardardFullDaysName());
+
+        return $this->getKhmerFullDaysName()[$dayIndex];
+    }
+
+    /**
+     * Get full day name in Khmer
+     *
+     * @return string
+     */
+    public function getFullDay() 
+    {
+        $dateNumber = date("d",strtotime($this->date));
+        return $this->khmerNumber[$dateNumber[0]]."".$this->khmerNumber[$dateNumber[1]];
+    }
+
+    /**
+     * Get full year 4 number in Khmer
+     *
+     * @return string
+     */
+    public function getFullYear() 
+    {
+        $khmerNumber = $this->getKhmerNumber();
+        $yearFullNumber = date("Y",strtotime($this->date));
+
+        $month = array_map(function($str) use ($khmerNumber) {
+            return $khmerNumber[$str];
+        }, str_split($yearFullNumber));
+
+        return join("",$month);
+    }
+
+    /**
+     * Get date base on format
+     *
+     * @param  string $revers
+     * @return string
+     */
+    public function getDate($revers = null) 
+    {
+        if ($this->isForwardSlah($this->date)) {
+            return $this->getForwardSlahFormat($revers);
+        }
+
+        if ($this->isDash($this->date)) {
+            return $this->getDashFormat($revers);
+        }
+
+        throw new \Exception("Undefined date format");
+    }
+
+    /**
+     * Checking if given date is a forward slash format
+     *
+     * @return boolean
+     */
+    public function isForwardSlah()
+    {
+        return is_object(DateTime::createFromFormat('Y/m/d', $this->date));
+    }
+
+    /**
+     * Checking if given date is a dash format
+     *
+     * @return boolean
+     */
+    public function isDash()
+    {
+        return is_object(DateTime::createFromFormat('Y-m-d', $this->date));
+    }
+
+    /**
+     * Get date in khmer format as Y/m/d
+     *
+     * @return string
+     */
+    public function getForwardSlahFormat($revers) 
+    {
+        return $this->format("/", $revers);
+    }
+
+    /**
+     * Get date in khmer format as Y-m-d
+     *
+     * @return string
+     */
+    public function getDashFormat($revers) 
+    {
+        return $this->format("-", $revers);
+    }
+
+    /**
+     * Return given date format
+     *
+     * @param  string $sign
+     * @return string
+     */
+    public function format($sign, $revers) 
+    {
+        if ($revers === "revers") {
+            return $this->getFullDay().$sign.$this->getFullMonth().$sign.$this->getFullYear();
+        }
+
+        return $this->getFullYear().$sign.$this->getFullMonth().$sign.$this->getFullDay();
+    }
+
+    /**
+     * Get khmer number from 0 to 9
+     *
+     * @return array
+     */
+    public function getKhmerNumber() 
+    {
+        return ['០', '១', '២', '៣', '៤', '៥', '៦', '៧', '៨', '៩'];
+    }
+
+    /**
+     * Get internation month in full nam
+     *
+     * @return aray
+     */
+    public function getInternationalMonth() 
+    {
+        return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September ', 'October', 'November', 'December'];
+    }
+
+    /**
+     * Get khmer month in full name
+     *
+     * @return array
+     */
+    public function getKhmerMonths() 
+    {
+        return ['មករា', 'កុម្ភៈ', 'មីនា', 'មេសា', 'ឧសភា', 'មិថុនា', 'កក្កដា', 'សីហា', 'កញ្ញា', 'តុលា', 'វិច្ឆិកា', 'ធ្នូ'];
+    }
+}
